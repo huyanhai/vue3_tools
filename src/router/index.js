31e190c4d074634f3c893e987412dd174d8e7c44
huyanhai <810839700@qq.com>
2021-03-29 11:46:11 +0800

    feat: 默认路由添加

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-const routes = [];
+const routes = [
+  { path: '/a', component: import('@/views/A.vue') },
+  { path: '/b', component: import('@/views/B.vue') },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
